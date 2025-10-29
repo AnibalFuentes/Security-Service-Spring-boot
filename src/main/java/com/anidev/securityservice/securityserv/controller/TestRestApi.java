d3f405ba651a748ed71b6279f8ac3d84865d03ad
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2025-10-29 15:37:19 -0500

    test api with security config

---
+++ src/main/java/com/anidev/securityservice/securityserv/controller/TestRestApi.java
@@ -0,0 +1,17 @@
+package com.anidev.securityservice.securityserv.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestRestApi {
+
+    @GetMapping
+    public Map<String,Object> dataTest() {
+
+        return Map.of("message","Data test");
+    }
+}
