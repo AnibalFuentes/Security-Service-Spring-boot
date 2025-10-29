577e18218506aea724c59290e5d7dea48cdcec99
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2025-10-29 15:36:54 -0500

    initial commit

---
+++ src/main/java/com/anidev/securityservice/securityserv/SecurityServApplication.java
@@ -0,0 +1,13 @@
+package com.anidev.securityservice.securityserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anidev/securityservice/securityserv/SecurityServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anidev.securityservice.securityserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
